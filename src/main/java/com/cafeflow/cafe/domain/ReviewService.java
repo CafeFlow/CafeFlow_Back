bfb6e2d847876c3a60d25c08de35a83d35079875
k1mm1nseok <k1mm1nseok@naver.com>
2023-11-06 22:14:26 +0900

    Update ReviewService.java
    에러표시 수정

--- src/main/java/com/cafeflow/cafe/domain/ReviewService.java
+++ src/main/java/com/cafeflow/cafe/domain/ReviewService.java
@@ -3,6 +3,7 @@ package com.cafeflow.cafe.domain;
 import com.cafeflow.cafe.domain.reviewDTO.ReviewEditDTO;
 import com.cafeflow.cafe.domain.reviewDTO.ReviewListDTO;
 import com.cafeflow.cafe.domain.reviewDTO.ReviewWriteDTO;
+import com.cafeflow.cafe.exception.NonExistentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,18 +27,16 @@ public class ReviewService {
 
     public Review editReview(Long id, ReviewEditDTO reviewDTO){
         Review modifiedReview = reviewRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("존재하지 않는 게시물입니다."));
+                new NonExistentException("존재하지 않는 게시물입니다."));
 
         modifiedReview.update(reviewDTO.getComments(), reviewDTO.getRate());
         return reviewRepository.save(modifiedReview);
     }
     public Review deleteReview(Long id){
         Review review = reviewRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("존재하지 않는 게시물입니다."));
+                new NonExistentException("존재하지 않는 게시물입니다."));
 
         reviewRepository.delete(review);
         return review;
     }
-
-
 }
